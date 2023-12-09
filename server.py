7c75a690f9ae5088c4b4a2303a069932381e460e
YulianaIvanova <valday-samara12@yandex.ru>
2023-12-09 20:10:51 +0400

    Add route.html, error_route.html and update prediction.html

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template,  request
 import requests
 import xml.etree.ElementTree as ET
 import os.path
+import hashlib
 counter = 0
 date_update = 0
 new_date_update = 0
@@ -60,11 +61,116 @@ def main():
                 stops_info.append(tmp)
         return render_template("stops.html", stops=stops_info)
 
-@app.route("/predication")
+@app.route("/prediction")
+def prediction():
+    global counter
+    counter += 1
+    KS_ID = request.args.getlist("stopId")
+    COUNT = 20
+
+    #create xml
+    first = ET.Element('request')
+    method = ET.SubElement(first, 'method')
+    parameters = ET.SubElement(first, 'parameters')
+    stop_id = ET.SubElement(parameters, 'KS_ID')
+    count = ET.SubElement(parameters, 'COUNT')
+    method.text = 'getFirstArrivalToStop'
+    stop_id.text = KS_ID[0]
+    count.text = str(COUNT)
+    
+    hash_object = hashlib.sha1(ET.tostring(first, encoding="utf-8") + 
+                               bytes("just_f0r_tests", encoding="utf-8")).hexdigest()
+
+    data = {"message": ET.tostring(first, encoding="utf-8"),
+            "os": "web",
+            "clientId": "test",
+            "authKey": hash_object}
+    
+    prediction = requests.post("https://tosamara.ru/api/v2/xml", data=data)
+    content = prediction.content.decode()
+    root = ET.fromstring(content)
+    predictions = []
+    for i in root:
+        if i[15].text == "Метро" or i[15].text =="Железная дорога":
+            i[16].text = '-'
+            i[1].text = '-'
+        result = {"id_transport": i[9].text,
+                  "time": i[12].text,
+                  "type": i[15].text,
+                  "number": i[6].text,
+                  "distance": i[16].text,
+                    "stop": i[1].text}
+        predictions.append(result)
+
+    check_update("stops.xml", 5)
+    stops = ET.parse("stops.xml")
+    stops_root = stops.getroot()
+    stop=""
+
+    for i in stops_root:
+        if KS_ID[0] == i[0].text:
+            if i[2].text == None:
+                i[2].text = ""
+            if i[3].text == None:
+                i[3].text == ""
+            stop = i[1].text + "\n" + i[2].text + " " + i[3].text
     
     
-    
+    return render_template("prediction.html", predictions=predictions, stop=stop)
 
 
+@app.route("/route")
+def route():
+    global counter
+    counter += 1
+    HULLNO = request.args.getlist("transportId")
+    first = ET.Element('request')
+    method = ET.SubElement(first, 'method')
+    parameters = ET.SubElement(first, 'parameters')
+    hullno = ET.SubElement(parameters, 'HULLNO')
+    method.text = 'getTransportPosition'
+    hullno.text = HULLNO[0]
+    hash_object = hashlib.sha1(ET.tostring(
+        first, encoding="utf-8") + bytes("just_f0r_tests", encoding="utf-8")).hexdigest()
+    data = {"message": ET.tostring(first, encoding="utf-8"),
+            "os": "web",
+            "clientId": "test",
+            "authKey": hash_object}
+    
+    route = requests.post("http://tosamara.ru/api/v2/xml", data=data)
+    with open("tmp.xml", "wb") as f:
+                f.write(route.content)
+    content = route.content.decode()
+    root_route = ET.fromstring(content)
+
+    if root_route.tag == "ExceptionReport":
+         return render_template("error_route.html")
+    route_list = []
+    route_id = root_route[0].text
+    check_update('routes.xml',2)
+    tree = ET.parse('routes.xml')
+    root_r = tree.getroot()
+    for i in root_r:
+        if route_id == i[0].text:
+            route_id = i[1].text + "  " +  i[6].text
+    
+    
+    for i in root_route:
+        for j in i:
+            result= {"stop_id":j[0].text, "time":str(round(float(j[1].text)/60))}
+            route_list.append(result)
+    check_update("stops.xml", 5)
+    tree = ET.parse('stops.xml')
+    root_stops = tree.getroot()
+    for i in route_list:
+        for j in root_stops:
+            if i["stop_id"] == j[0].text:
+                i["stop_id"] = j[1].text
+    #print(route_list)
+   
+    
+    return render_template("route.html", route_id=route_id, route_list=route_list) 
+    
+    
 if __name__ == "__main__":
     app.run(debug=True)
